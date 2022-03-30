6e9f1243151cdf4ee306cfb44b064bce6175908c
Nubb <nubb@forestblock.net>
2022-03-30 04:25:38 -0600

    ip copies to clipboard

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-function clipboard() {
-    var copyText = document.getElementById("headertext")
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+})
 
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-
-    navigator.clipboard.writeText(copyText.value);
-
-    alert("Copied the text :" + copyText.Value);
+function CopyToClipboard(id) {
+var r = document.createRange();
+    r.selectNode(document.getElementById(id));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(r);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
 }
+
+new PlayerCounter({
+    element: element,
+    ip: 'server ip',
+    format: '{online}/{max}', // default {online}
+    refreshRate: 60 * 1000 // default 1m
+  });
