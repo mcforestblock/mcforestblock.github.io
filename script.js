9ff9d043de0b56e4979486067df8d337e0886f21
Nubb <nubb@forestblock.net>
2022-04-19 23:30:22 -0600

    New header design

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
-
 var loader = document.getElementById("preloader");
-window.addEventListener("load", function(){
+window.addEventListener("load", function() {
     loader.style.display = "none";
 })
 
 
-function CopyToClipboard(id) {
-var r = document.createRange();
-    r.selectNode(document.getElementById(id));
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(r);
-    document.execCommand('copy');
-    window.getSelection().removeAllRanges();
+// function CopyToClipboard(id) {
+//     var r = document.createRange();
+//     r.selectNode(document.getElementById(id));
+//     window.getSelection().removeAllRanges();
+//     window.getSelection().addRange(r);
+//     document.execCommand('copy');
+//     window.getSelection().removeAllRanges();
 
-    alert("Copied Server Address");
+//     alert("Copied Server Address");
+// }
+
+// function myFunction() {
+//     var copyText = document.getElementById("serverip");
+
+//     navigator.clipboard(copyText.value);
+
+//     var tooltip = document.getElementById("myTooltip");
+//     tooltip.innerHTML = "Copied to Clipboard";
+// }
+
+function copyToClipboard(text) {
+    const elem = document.createElement('mc.forestblock.net');
+    elem.value = text;
+    document.body.appendChild(elem);
+    elem.select();
+    document.execCommand('copy');
+    document.body.removeChild(elem);
+}
+
+function outFunc() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Click to Copy";
 }
 
 function navion() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
-      x.className += " responsive";
+        x.className += " responsive";
     } else {
-      x.className = "topnav";
+        x.className = "topnav";
     }
-  }
-
+}
